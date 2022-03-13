95cc6d6feb3bd63bcd81789c760c3b77e4e91414
jaehomersimpson <p5564989@gmail.com>
2022-03-13 23:57:36 +0900

    Tabhost
    Tabhost practwice - 1

---
+++ LayoutPractice/app/src/main/java/com/example/layoutpractice/Tabhost.java
@@ -0,0 +1,8 @@
+package com.example.layoutpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tabhost extends AppCompatActivity {
+
+    TabHost host = findViewById(R.id.host)
+}
