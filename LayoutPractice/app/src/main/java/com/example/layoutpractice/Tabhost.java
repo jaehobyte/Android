767817c123db3198ac55c4f7d06dbb153bce2ac3
jaehomersimpson <p5564989@gmail.com>
2022-03-14 15:47:46 +0900

    Tabhost Practice2
    Tabhost Practice2

--- LayoutPractice/app/src/main/java/com/example/layoutpractice/Tabhost.java
+++ LayoutPractice/app/src/main/java/com/example/layoutpractice/Tabhost.java
@@ -1,8 +1,37 @@
 package com.example.layoutpractice;
 
+import android.os.Bundle;
+import android.widget.TabHost;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 public class Tabhost extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabhost);
 
-    TabHost host = findViewById(R.id.host)
+        TabHost host = findViewById(R.id.host);
+        host.setup();
+
+        Tabhost.TabSpec spec = host.newTabSpec("tab1");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(
+                getResources(), R.drawable.tab_icon1, null));
+        spec.setContent(R.id.tab_content1);
+        host.addTab(spec);
+
+        spec=host.newTabSpec("tab2");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(
+                getResources(), R.drawable.tab_icon2, null));
+        spec.setContent(R.id.tab_content2);
+        host.addTab(spec);
+
+        spec=host.newTabSpec("tab3");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(
+                getResources(), R.drawable.tab_icon3, null));
+        spec.setContent(R.id.tab_content3);
+        host.addTab(spec);
+
+    }
 }
